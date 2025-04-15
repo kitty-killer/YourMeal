447a6bb5ec91751b78a49dfb93f856250ffc2f30
kitty-killer <124335721+kitty-killer@users.noreply.github.com>
2025-04-15 18:47:49 +0300

    router

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,15 +1,10 @@
-import './App.scss'
-import Header from '../components/header/header.jsx'
-import Main from '../components/main/main.jsx'
-import Footer from '../components/footer/footer.jsx'
+import './App.css';
+import {Header} from "../components/header/header.jsx";
 
-export default function App(){
-
-  return(
-    <>
-      <Header/>
-      <Main/>
-      <Footer/>
-    </>
-  )
-}
+export const App = () => {
+    return (
+        <>
+            <Header />
+        </>
+    );
+};
